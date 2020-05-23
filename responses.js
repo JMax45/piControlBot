f722f9ed41565bf1af616e23eeb51f927c1c7d4a
JMax45 <maxmailru.45@gmail.com>
2020-05-23 19:40:33 +0200

    Password generator in scene

--- responses.js
+++ responses.js
@@ -121,55 +121,15 @@ class Responses{
             ctx.replyWithPhoto( {source: Buffer.from(url.replace('data:image/png;base64,', ''), 'base64')} )
         })
     }
-    password(ctx, passGenerator, markup){
-        if(ctx.state.command.args.length == 0){
-            const message = "Per generare le password usa il seguente formato:\n`/password length numbers symbols lowercase/upercase`\n_Esempio:_ `/password 10 y n lowercase`"
-            ctx.reply(message, markup)
-        }
-        else{
-            console.log("Received password generation request")
-            const parameters = [];
-            for(let i=0; i<ctx.state.command.splitArgs.length; i++){
-                parameters.push(ctx.state.command.splitArgs[i].replace("y", true).replace("n", false))
-            }
-            for(let i=1; i<parameters.length-1; i++){
-                if(parameters[i]=="true"){
-                    parameters[i] = true;
-                }
-                else{
-                    parameters[i] = false;
-                }
-            }
-            // Checks if all parameters are false
-            let check = false;
-            for(let i=0; i<parameters.length; i++){
-                if(parameters[i]==true){
-                    check = true;
-                }
-            }
-            if(check == true){
-                if(parameters[3]=="lowercase"){
-                    parameters[3] = true;
-                    parameters[4] = false;
-                }
-                else{
-                    parameters[3] = false;
-                    parameters[4] = true;
-                }
-                const password = passGenerator.generate({
-                    length: ctx.state.command.splitArgs[0],
-                    numbers: parameters[1],
-                    symbols: parameters[2],
-                    lowercase: parameters[3],
-                    uppercase: parameters[4]
-                });
-                ctx.reply(password)
-                console.log("Password sent")
-            }
-            else{
-                ctx.reply("*Almeno uno dei parametri deve essere true*", markup)
-            }
-        }    
+    getPassword(ctx, passGenerator, markup, parameters){
+        const password = passGenerator.generate({
+            length: parseInt(parameters.characters, 10),
+            numbers: parameters.numbers,
+            symbols: parameters.symbols,
+            lowercase: parameters.lowerCase,
+            uppercase: parameters.upperCase
+        });
+        ctx.reply(password)
     }
     text(ctx){
         if(ctx.state.command==undefined){
