ede547f54c017f41bfec21da82863181f407d9e1
JMax45 <maxmailru.45@gmail.com>
2020-08-08 11:11:19 +0200

    Audio command.

--- src/Bot.js
+++ src/Bot.js
@@ -7,6 +7,10 @@ class Bot{
 		this.Telegraf = require('telegraf');
 		this.telegraf = new this.Telegraf(this.configuration.token);
 
+		// Command parts
+		this.commandParts = require('telegraf-command-parts');
+		this.telegraf.use(this.commandParts());
+
 		// JMongo
 		const JMongo = require('jmongo');
 		const jmongo = new JMongo(this.configuration.jmongo.url, this.configuration.jmongo.name);
