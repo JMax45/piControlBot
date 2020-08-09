19d64d83aba7516103fdf1fa2d76f3fe0d4e57bc
JMax45 <maxmailru.45@gmail.com>
2020-08-09 16:59:54 +0200

    Protected numbers.

--- src/commands/sms.js
+++ src/commands/sms.js
@@ -1,33 +1,44 @@
 module.exports = {
     name: 'sms',
-    execute(ctx){
-        const { spawn } = require("child_process");
+    execute(ctx, params){
+        const { spawn, exec } = require("child_process");
+        const { jmongo } = params;
 
         const args = ctx.state.command.splitArgs;
-        const ls = spawn("quack", ["--tool", "sms", "--target", args[0], "--timeout", args[1], "--threads", args[2]]);
-        ctx.reply(`Started sms bombing on ${args[0]}`);
-        
-        let amount = 0;
 
-        ls.stdout.on("data", data => {
-            const split = String(data).split(/\r?\n/);
-            for(let i=0; i<split.length; i++){
-                if(split[i].indexOf('[+]')!=-1){
-                    amount++;
-                }
+        jmongo.load('bannedNumbers', { number: args[0] }, (result) => {
+            if(result===null){
+                const ls = spawn("quack", ["--tool", "sms", "--target", args[0], "--timeout", args[1], "--threads", args[2]]);
+                ctx.reply(`Started sms bombing on ${args[0]}`);
+                executeBombing(ls);
             }
-        });
+            else{
+                ctx.replyWithMarkdown(`\`${args[0]}\` is a protected number, it belongs to *${result.name}*`)
+            }
+        })
 
-        ls.stderr.on("data", data => {
-            ctx.reply('There was an error: ' + data);
-        });
+        function executeBombing(ls){
+            let amount = 0;
+            ls.stdout.on("data", data => {
+                const split = String(data).split(/\r?\n/);
+                for(let i=0; i<split.length; i++){
+                    if(split[i].indexOf('[+]')!=-1){
+                        amount++;
+                    }
+                }
+            });
 
-        ls.on('error', (error) => {
-            ctx.reply('There was an error: ' + error.message);
-        });
+            ls.stderr.on("data", data => {
+                ctx.reply('There was an error: ' + data);
+            });
 
-        ls.on("close", code => {
-            ctx.reply(`Bombing completed, ${amount} messages sent`);
-        });
+            ls.on('error', (error) => {
+                ctx.reply('There was an error: ' + error.message);
+            });
+
+            ls.on("close", code => {
+                ctx.reply(`Bombing completed, ${amount} messages sent`);
+            });
+        }    
     }
 }
