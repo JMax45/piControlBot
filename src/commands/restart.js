16c510de35d83a12c3d29f9072e0caa55e451c42
JMax45 <maxmailru.45@gmail.com>
2020-08-14 10:52:57 +0200

    Private access to restart and shutdown.

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -1,5 +1,8 @@
 module.exports = {
     name: 'restart',
+    description: 'Restart the system',
+    public: true,
+    access: 'private',
     execute(ctx){
         require('child_process').exec('sudo /sbin/shutdown now -r', function (msg) { ctx.reply(msg) });
     }
--- src/commands/shutdown.js
+++ src/commands/shutdown.js
@@ -1,5 +1,8 @@
 module.exports = {
     name: 'shutdown',
+    description: 'Shutdown the system',
+    public: true,
+    access: 'private',
     execute(ctx){
         require('child_process').exec('sudo /sbin/shutdown now', function (msg) { ctx.reply(msg) });
     }
